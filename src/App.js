c961e9f04223acd73588397eca32999bafbafc3b
jean.coding971@gmail.com <email@address.com>
2022-06-20 13:09:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
