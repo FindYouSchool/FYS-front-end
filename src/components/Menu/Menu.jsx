480baeefc9810917c0b4a1e5169c7a95359a1b0b
jean.coding971@gmail.com <email@address.com>
2022-06-21 20:42:26 +0200

    add files

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Avatar from "../Avatar/Avatar";
+import "./Menu.css";
+
+const Menu = () => {
+  const { isAuthenticated } = useAuth();
+  const [showLinks, setShowlinks] = useState(false);
+  const navigate = useNavigate();
+
+  const handleShowLinks = () => {
+    setShowlinks(!showLinks);
+  };
+  const handleClick = () => {
+    if (showLinks) {
+      setShowlinks(false);
+    }
+  };
+  return (
+    <nav className={`menu-container ${showLinks ? "show-nav" : "hide-nav"} `}>
+      <div className="logo-container fw-bold ">Logo</div>
+      <ul className="navbar-links">
+        <NavLink
+          to="/"
+          onClick={handleClick}
+          className="navbar-link d-inline-block mx-3"
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/schools"
+          onClick={handleClick}
+          className="navbar-link d-inline-block mx-3"
+        >
+          Ecoles
+        </NavLink>
+        <NavLink
+          to="/notice"
+          onClick={handleClick}
+          className="navbar-link d-inline-block mx-3"
+        >
+          Donner son avis
+        </NavLink>
+        {isAuthenticated ? (
+          <Avatar
+            size={30}
+            containerStyle="avatar-container"
+            className="avatar-opened-nav"
+            onClick={() => {
+              handleClick();
+              navigate("/profile");
+            }}
+          />
+        ) : (
+          <NavLink
+            to="/login"
+            onClick={handleClick}
+            className="navbar-link d-inline-block mx-3"
+          >
+            Se connecter
+          </NavLink>
+        )}
+      </ul>
+      <button className="navbar-burger" onClick={handleShowLinks}>
+        <span className="burger-bar"></span>
+      </button>
+    </nav>
+  );
+};
+
+export default Menu;
